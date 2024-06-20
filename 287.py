5aaa4dba1601250acf6fa38fc768108bbf9efb8a
tom tait <thomas.t.ca@outlook.com>
2024-06-20 19:01:22 -0300

    2619 287

---
+++ 287.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findDuplicate(self,nums):
+        hashmap = {}
+        for num in nums:
+            if (num in hashmap):
+                return num
+            else:
+                hashmap[num] = 1
+    
+
+
+
+    
+
+sol = Solution()
+nums = [1,3,4,2,2]
+print(sol.findDuplicate(nums))
